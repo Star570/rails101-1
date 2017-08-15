43bbfef416e538fab17de22e2a71ff76b556ce5f
ys <cee570@gmail.com>
2017-08-15 22:23:53 +0800

    use simple_format helper to decorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,51 @@
 class PostsController < ApplicationController
-	before_filter :authenticate_user!, :only => [:new, :create]
+	before_filter :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
+	before_action :find_post_and_check_permission, only: [:edit, :update, :destroy]
 
-	 def new
-	 	@group = Group.find(params[:group_id])
-	 	@post = Post.new
-	 end
+	def new
+		@group = Group.find(params[:group_id])
+		@post = Post.new
+	end
 
-	 def create
-	 	@group = Group.find(params[:group_id])
-	 	@post = Post.new(post_params)
-	 	@post.group = @group
-	 	@post.user = current_user
-	 	if @post.save
-	 		redirect_to group_path(@group)
-	 	else
-	 		render :new
-	 	end
-	 end
+	def create
+		@group = Group.find(params[:group_id])
+		@post = Post.new(post_params)
+		@post.group = @group
+		@post.user = current_user
+		if @post.save
+			redirect_to group_path(@group)
+		else
+			render :new
+		end
+	end
 
-	 	private
+	def edit
+	end
 
-	 	def post_params
-	 		params.require(:post).permit(:content)
-	 	end
+	def update
+		if @post.update(post_params)
+			redirect_to account_posts_path, notice: "Update Success"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to account_posts_path, alert: "Post deleted"
+	end
+
+	private
+
+	def find_post_and_check_permission
+		@group = Group.find(params[:group_id])
+		@post = Post.find(params[:id])
+		if current_user != @post.user
+			redirect_to root_path, alert: "You have no permission."
+		end
+	end
+
+	def post_params
+		params.require(:post).permit(:content)
+	end
 end
